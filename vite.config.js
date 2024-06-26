10b15eb79c9d4a9def6b605d2f11468cc8acdc21
ValeriaMyronets <valery.mir.11@gmail.com>
2024-06-26 21:58:01 +0300

    added base url

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/aivia_test_task_demo/',
   plugins: [
     vue(),
     vueJsx(),
