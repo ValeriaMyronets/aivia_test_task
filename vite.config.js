16e9ceb57535bb9884bcb715b44cd942459b7d67
ValeriaMyronets <valery.mir.11@gmail.com>
2024-06-26 22:10:16 +0300

    deploy.sh and removed devTools

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/aivia_test_task/',
   plugins: [
     vue(),
-    vueJsx(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
